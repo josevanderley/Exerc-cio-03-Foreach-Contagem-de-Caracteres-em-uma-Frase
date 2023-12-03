83c2e1e249a147948ac71d8d638f1dca2a5db21b
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:51:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_03_Foreach__Contagem_de_Caracteres_em_uma_Frase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************************BEM VINDO AO PROGRAMA*****************************");
+
+            string frase;
+            int contadorVogais = 0;
+
+            Console.WriteLine("Digite uma frase: ");
+            frase = Console.ReadLine();
+
+            foreach (char caractere in frase)
+            {
+                if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u')
+                {
+                    contadorVogais++;
+                }
+            }
+
+            Console.WriteLine("A frase contém {0} vogais.", contadorVogais);
+
+            Console.ReadKey();
+
+            Console.WriteLine("Digite 1 para sair");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
+    
